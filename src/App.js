2545af9e63e69065086881ce2678a021fa7c34bd
Carlos Melegrito <caltech511@gmail.com>
2021-09-08 20:52:05 -0300

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <div>
+      
+      <h1>Project</h1>
+    </div>
+  );
+}
+
+export default App;
